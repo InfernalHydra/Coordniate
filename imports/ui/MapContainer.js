0301ca2e28b728ac2ada07b79b294cffc2df9fae
Ryan Chhong <ryanchhong@gmail.com>
2018-02-24 16:19:58 -0600

    Made pins show up when coords added

--- imports/ui/MapContainer.js
+++ imports/ui/MapContainer.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, Marker} from 'google-maps-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Events } from '../api/events.js';
 
-export class MapContainer extends Component {
+class MapContainer extends Component {
+  renderMarkers()
+  {
+    //Events.insert({ lat : 15, lng : 15 });
+    if(this.props.events.length == 0)
+    {
+      return;
+    }
+    console.log(this.props.events);
+    return this.props.events.map((event) => (
+      <Marker key = {event.lat * event.lng} position = {{lat : event.lat, lng : event.lng}}/>
+    ));
+  }
+
   render() {
     return (
       <Map google = {this.props.google} zoom = {8}>
-      </Map>
+        <Marker position = {{lat: 29.7604, lng: -95.3698}}/>  
+        {this.renderMarkers()}
+    </Map>
     );
   }
+
+
 }
-export default GoogleApiWrapper({
-  apiKey : 'AIzaSyBmlSJGyprhqkW2ry1KEwTZGdzoFURGQ4A'
-})(MapContainer)
+
+export default withTracker(() => {
+  Meteor.subscribe('events');
+  return {
+    events: Events.find({}).fetch()
+  };
+})(MapContainer);
