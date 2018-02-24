9ed64628cb9fad203e1fbdaebfd3bce6b2060590
TheBunanna <thebunanna@gmail.com>
2018-02-24 10:29:03 -0600

    stuff
    random

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
 
 export default class App extends Component
 {
+  constructor(props){
+    super(props);
+    this.state = {color: red};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    let color = this.state.color == "red" ? "blue" : "red";
+    this.setState({color: color});
+  }
   render() {
     return (
-      <div>{this.props.name}</div>
+      <button onClick={handleClick}> CLICK MEH </button>
     );
   }
 }
